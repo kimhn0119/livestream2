d8340762270d2d45c69fd08305f046be332d0355
kimhn0119 <sky011900@gmail.com>
2023-12-30 10:49:28 +0100

    Update get-started.tsx

--- src/components/get-started.tsx
+++ src/components/get-started.tsx
@@ -46,7 +46,7 @@ export default function HomeForm() {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="secondary" disabled={!validSlug}>
-            Join as host
+            Join as host ddddddd
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
